4d45ad220ba7d5d31db8cce6783528d66e26cae9
”erchong <1169746147@qq.com>
2018-05-25 18:11:11 +0800

    '去掉多余文件'

--- entryBuild/demo.js
+++
@@ -1,6 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../app/compontent/demo/Index';
-
-ReactDOM.render(<App />, document.getElementById('app'));
--- entryBuild/shop.js
+++
@@ -1,6 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../app/compontent/shop/Index';
-
-ReactDOM.render(<App />, document.getElementById('app'));
